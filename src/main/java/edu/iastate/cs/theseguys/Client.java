a968178cba73be3a04499b1013c596f21fd4a873
Elliott Butler <elliottmbutler@gmail.com>
2016-03-29 20:11:50 -0500

    Update some stuff, exciting commit message

---
+++ src/main/java/edu/iastate/cs/theseguys/Client.java
@@ -0,0 +1,11 @@
+package edu.iastate.cs.theseguys;
+
+public class Client {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+
+    public static void main() {
+        
+    }
+}
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,13 @@
+import edu.iastate.cs.theseguys.Client;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ClientTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        Client classUnderTest = new Client();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
