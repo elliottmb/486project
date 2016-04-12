5a785d1c285cb62fb29987ec610112f67487de5a
Alex Dana <acdana@iastate.edu>
2016-04-12 16:45:04 -0500

    Updated security files

--- shared/src/main/java/edu/iastate/cs/theseguys/security/AuthoritySecurity.java
+++ shared/src/main/java/edu/iastate/cs/theseguys/security/AuthoritySecurity.java
@@ -1,29 +1,52 @@
 package edu.iastate.cs.theseguys.security;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.Signature;
-import java.security.SignatureException;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
 
 public class AuthoritySecurity {
 	
-	public boolean verifySignature(byte[] message, byte[] sig, PublicKey publicKey) {
-        Signature signature;
+	private PrivateKey privateKey; //TODO need to store this somewhere once it is created
+	private PublicKey publicKey; //TODO need to make this key available to users
+	
+
+	public boolean generateKeyPair() {
 		try {
-			signature = Signature.getInstance("SHA256withRSA");
-	        signature.initVerify(publicKey);
-	        signature.update(message);
-	        return signature.verify(sig);
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			kpg.initialize(1024, random);
+			KeyPair pair = kpg.generateKeyPair();
+			
+			privateKey = pair.getPrivate();
+			publicKey = pair.getPublic();
+			//TODO store keys
+			return true;
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return false;	
+	}
+	
+	public byte[] generateSignature(String message, PrivateKey priv) {
+		try {
+			Signature sig = Signature.getInstance("SHA256withRSA");
+			sig.initSign(priv);
+			sig.update(message.getBytes("UTF-8"));
+			return sig.sign();
+			
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (InvalidKeyException e) {
 			e.printStackTrace();
 		} catch (SignatureException e) {
 			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 		}
-		return false;
+		
+		return null;
+		
 	}
 	
-
+	
 }
