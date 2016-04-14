3c889bad6536b5215e32b1772b5b9b043eb5ca6b
Elliott Butler <elliottmbutler@gmail.com>
2016-04-13 19:54:29 -0500

    Initial processing queue work
    May move this to DatabaseManager, not set in stone in current placement

--- client/src/main/java/edu/iastate/cs/theseguys/distributed/ClientManager.java
+++ client/src/main/java/edu/iastate/cs/theseguys/distributed/ClientManager.java
@@ -1,6 +1,8 @@
 package edu.iastate.cs.theseguys.distributed;
 
 import edu.iastate.cs.theseguys.AbstractIoConnectorManager;
+import edu.iastate.cs.theseguys.DatabaseManager;
+import edu.iastate.cs.theseguys.database.MessageRecord;
 import edu.iastate.cs.theseguys.network.*;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
@@ -13,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 @Component
 public class ClientManager extends AbstractIoConnectorManager {
@@ -20,12 +24,40 @@ public class ClientManager extends AbstractIoConnectorManager {
 
     @Autowired
     private LoggingMessageHandler loggingMessageHandler;
+    @Autowired
+    private DatabaseManager databaseManager;
 
+    private Deque<MessageDatagram> waiting;
+    private Deque<MessageDatagram> ready;
     @Autowired
     private LatestMessageResponseHandler latestMessageResponseHandler;
 
     public ClientManager() {
         super(new NioSocketConnector(), new ClientDemuxingIoHandler());
+
+        waiting = new ConcurrentLinkedDeque<>();
+        ready = new ConcurrentLinkedDeque<>();
+    }
+
+    public synchronized void processQueue() {
+        if (!waiting.isEmpty()) {
+            MessageDatagram head = waiting.pop();
+
+            // Make sure it doesn't already exist
+            if (!databaseManager.getRepository().exists(head.getId())) {
+                // Check if we have both the father and mother already
+                if (databaseManager.getRepository().exists(head.getFatherId()) && databaseManager.getRepository().exists(head.getMotherId())) {
+                   // If we do, move this guy to the ready queue
+                    ready.add(head);
+                } else {
+                    // TODO:
+                    waiting.add(head);
+                }
+            }
+        }
+        if (!ready.isEmpty()) {
+
+        }
     }
 
     @PostConstruct
