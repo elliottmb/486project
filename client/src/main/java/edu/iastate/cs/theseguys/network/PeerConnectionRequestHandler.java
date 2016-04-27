4e4b6d6588b445ce5c6f65bb7bcba6309db7c734
Elliott Butler <elliottmbutler@gmail.com>
2016-04-26 19:32:40 -0500

    Include updated handled for previous commit

--- client/src/main/java/edu/iastate/cs/theseguys/network/PeerConnectionRequestHandler.java
+++ client/src/main/java/edu/iastate/cs/theseguys/network/PeerConnectionRequestHandler.java
@@ -1,8 +1,6 @@
 package edu.iastate.cs.theseguys.network;
 
-import java.net.InetSocketAddress;
-import java.util.Map.Entry;
-
+import edu.iastate.cs.theseguys.distributed.ClientManager;
 import org.apache.mina.core.RuntimeIoException;
 import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.core.session.IoSession;
@@ -12,42 +10,44 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import edu.iastate.cs.theseguys.distributed.ClientManager;
+import java.net.InetSocketAddress;
 
 @Component
 public class PeerConnectionRequestHandler implements MessageHandler<PeerConnectionRequest> {
-
     private static final Logger log = LoggerFactory.getLogger(PeerConnectionRequestHandler.class);
 
-	
-	@Autowired
-	private ClientManager clientManager;
-	
-	@Override
-	public void handleMessage(IoSession session, PeerConnectionRequest message) throws Exception {
-		
-		log.info("PEER CONNECTION REQUEST RECEIVED WITH SESSION ID:"+message.getSessionId()+".  LISTING ALL CONNECTED CLIENTS");
-		
-		for(Entry<Long, IoSession> io : clientManager.getService().getManagedSessions().entrySet())
-		{
-			System.out.println("CONNECTED CLIENT SESSIONID: "+io.getKey());
-		}
-		
-		ConnectFuture peerFuture;
-		try {
-			//if we are not already connected to the requesting client.  
-			if (!clientManager.getService().getManagedSessions().containsKey(message.getSessionId()))
-			{
-				peerFuture = clientManager.connect(new InetSocketAddress(((InetSocketAddress)session.getRemoteAddress()).getHostName(), message.getServerPort()));
-	            peerFuture.awaitUninterruptibly();
-			}  
+    @Autowired
+    private ClientManager clientManager;
+
+    @Override
+    public void handleMessage(IoSession session, PeerConnectionRequest request) throws Exception {
+        InetSocketAddress sessionAddress = (InetSocketAddress) session.getRemoteAddress();
+
+        InetSocketAddress sessionServerAddress = new InetSocketAddress(sessionAddress.getHostName(), request.getServerPort());
+
+        log.info("Received " + request + " from " + sessionAddress);
+
+        ConnectFuture peerFuture;
+        try {
+            //if we are not already connected to the requesting client.
+            boolean alreadyConnected = clientManager.getService()
+                    .getManagedSessions()
+                    .entrySet()
+                    .stream()
+                    .anyMatch(
+                            e -> {
+                                InetSocketAddress eAddress = (InetSocketAddress) e.getValue().getRemoteAddress();
+                                return eAddress.equals(sessionServerAddress);
+                            }
+                    );
+            if (!alreadyConnected) {
+                peerFuture = clientManager.connect(sessionServerAddress);
+                peerFuture.awaitUninterruptibly();
+            }
         } catch (RuntimeIoException e) {
             log.warn("Failed to connect to peer");
             e.printStackTrace();
         }
-		
-		//clientManager.connect(new InetSocketAddress(((InetSocketAddress)session.getLocalAddress()).getHostName(), message.getServerPort()));
-		
-	}
+    }
 
 }
