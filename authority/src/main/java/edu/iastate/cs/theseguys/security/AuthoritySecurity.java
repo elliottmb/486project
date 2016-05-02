1dfd666536a9609ee4063531edcd7936633d4356
Elliott Butler <elliottmbutler@gmail.com>
2016-05-01 19:22:45 -0500

    Log all the verification steps..

--- authority/src/main/java/edu/iastate/cs/theseguys/security/AuthoritySecurity.java
+++ authority/src/main/java/edu/iastate/cs/theseguys/security/AuthoritySecurity.java
@@ -1,12 +1,17 @@
 package edu.iastate.cs.theseguys.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 
 public class AuthoritySecurity {
+    private static final Logger log = LoggerFactory.getLogger(AuthoritySecurity.class);
 
 /*	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
         AuthoritySecurity a = new AuthoritySecurity();
@@ -17,14 +22,21 @@ public class AuthoritySecurity {
 	}*/
 
     public boolean generateKeyPair() {
-        try {
-            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            kpg.initialize(1024, random);
-            KeyPair pair = kpg.generateKeyPair();
-
+        log.info("Checking if files exist");
+        if (!new File("pub.key").isFile() && !new File("priv.key").isFile()) {
             try {
-                if (!new File("pub.key").isFile() && !new File("priv.key").isFile()) {
+                log.info("Getting KeyPairGenerator");
+                KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+                log.info("Getting SecureRandom");
+                SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+                log.info("Initializing KeyPairGenerator");
+                kpg.initialize(1024, random);
+                log.info("Generating Key Pair");
+                KeyPair pair = kpg.generateKeyPair();
+
+                try {
+
+                    log.info("Files do not exist, creating");
                     FileOutputStream keyOutput = new FileOutputStream("pub.key");
                     keyOutput.write(pair.getPublic().getEncoded());
                     keyOutput.close();
@@ -33,23 +45,30 @@ public class AuthoritySecurity {
                     keyOutput.write(pair.getPrivate().getEncoded());
                     keyOutput.close();
                     return true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    log.warn("IOException");
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+                log.warn("NoSuchAlgorithmException");
+            }
         }
+        log.info("Files do exist, returning");
         return false;
     }
 
     public byte[] generateSignature(String message, PrivateKey priv) {
         try {
+            log.info("Generating signature for message: " + message);
+            log.info("Message in bytes" + Arrays.toString(message.getBytes("UTF-8")));
             Signature sig = Signature.getInstance("SHA256withRSA");
             sig.initSign(priv);
             sig.update(message.getBytes("UTF-8"));
-            return sig.sign();
+            byte[] signature = sig.sign();
+            log.info("Generated signature: " + Arrays.toString(signature));
+            return signature;
 
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
--- client/src/main/java/edu/iastate/cs/theseguys/security/ClientSecurity.java
+++ client/src/main/java/edu/iastate/cs/theseguys/security/ClientSecurity.java
@@ -1,5 +1,7 @@
 package edu.iastate.cs.theseguys.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
@@ -7,6 +9,7 @@ import java.security.*;
 
 @Component
 public class ClientSecurity {
+    private static final Logger log = LoggerFactory.getLogger(ClientSecurity.class);
 
     public boolean verifySignature(String message, byte[] sig, PublicKey publicKey) {
         Signature signature;
@@ -17,12 +20,16 @@ public class ClientSecurity {
             return signature.verify(sig);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
+            log.warn(e.toString());
         } catch (InvalidKeyException e) {
             e.printStackTrace();
+            log.warn(e.toString());
         } catch (SignatureException e) {
             e.printStackTrace();
+            log.warn(e.toString());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
+            log.warn(e.toString());
         }
         return false;
     }
